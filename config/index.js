68161f5bcce1b811998ab1101e6bd20fbfb188ca
Poon Nguyen <nguyenduypoon@gmail.com>
2019-06-26 12:01:26 +0700

    test

---
+++ config/index.js
@@ -0,0 +1,14 @@
+module.exports = {
+  port: 443,
+  expire_timeout: 5000,
+  key: 'poondeptrai',
+  path: '/memlovea1',
+  concurrent_limit: 5000,
+  allow_discovery: false,
+  proxied: false,
+  cleanup_out_msgs: 1000,
+  ssl: {
+    key: '',
+    cert: ''
+  }
+};
